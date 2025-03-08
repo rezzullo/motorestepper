f40842f03a4d6b2d57fcb85469e38777c5e8ea28
rezzullo <andrea.rezzani19@gmail.com>
2025-03-08 09:41:00 +0100

    .

--- generatore-qrcode.py
+++ generatore-qrcode.py
@@ -1,5 +1,5 @@
 import qrcode
-local_url = "http://localhost:8000/documentazione_progetto.html"
+local_url = "" #your url 
 qr = qrcode.make(local_url)
 qr.save("qrcode_documentazione_progetto.png")
 print("QR code generato e salvato come 'qrcode_documentazione.png'")
