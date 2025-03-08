7df5c1fe78ad495b56d955d1e6cf0279bf4245cc
rezzullo <andrea.rezzani19@gmail.com>
2025-03-08 09:38:19 +0100

    .

--- esp32_paginaweb-mDNS-motorestepper.ino
+++ esp32_paginaweb-mDNS-motorestepper.ino
@@ -1,36 +1,36 @@
-#include <WiFi.h>
+/*
+This project is made for Esp32 microcontroller board using Arduino IDE
+*/
+
+#include <WiFi.h>         //libraries
 #include <WebServer.h>
 #include <ESPmDNS.h>
 
-// Credenziali WiFi
-const char* ssid = "iPhone di rezzullo";
-const char* password = "patrizia";
+const char* ssid = "your_ssid";           //access to WiFi
+const char* password = "your_password";
 
-// Server Web
 WebServer server(80);
 
-// Definizione pin del motore stepper
-#define IN1 12
+#define IN1 12    //motor pin definition, changeable based  on pins used 
 #define IN2 14
 #define IN3 27
 #define IN4 26
 
-// Sequenze per i diversi metodi di funzionamento
-const int waveDrive[4][4] = {
+const int waveDrive[4][4] = {   //waveDrive
   {1, 0, 0, 0}, //ab
   {0, 0, 1, 0}, //cd
   {0, 1, 0, 0}, //ba
   {0, 0, 0, 1} //dc
 };
 
-const int fullStep[4][4] = {
+const int fullStep[4][4] = {   //fullStepDrive
   {1, 0, 1, 0}, //ab cd  
   {0, 1, 1, 0}, //ba cd
   {0, 1, 0, 1}, //ba dc
   {1, 0, 0, 1}  //ab dc
 };
 
-const int halfStep[8][4] = {
+const int halfStep[8][4] = {   //halfStepDrive
   {1, 0, 0, 0}, //ab
   {1, 0, 1, 0}, //ab cd
   {0, 0, 1, 0}, //cd
@@ -41,14 +41,14 @@ const int halfStep[8][4] = {
   {1, 0, 0, 1}  //ab dc
 };
 
-// Pagina Web HTML
+// HTML web page editable
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Controllo Stepper</title>
+  <title>Controllo motore stepper</title>  //title web page
   <script>
-    function invia() {
+    function invia() { //function to send steps to web page
       var passi = document.getElementById("passi").value;
       var metodo = document.getElementById("metodo").value;
       fetch("/stepper?passi=" + passi + "&metodo=" + metodo);
@@ -58,7 +58,7 @@ const char index_html[] PROGMEM = R"rawliteral(
 <body>
   <h2>Controllo Motore Stepper</h2>
   <label>Numero di passi:</label>
-  <input type="number" id="passi" value="100">
+  <input type="number" id="passi" value="100"> //input steps
   
   <label>Metodo di funzionamento:</label>
   <select id="metodo">
@@ -71,13 +71,12 @@ const char index_html[] PROGMEM = R"rawliteral(
 </html>
 )rawliteral";
 
-// Funzione per muovere lo stepper
-void moveStepper(int passi, String metodo) {
-  int delayTime = 20;  // Velocità del motore
+void moveStepper(int passi, String metodo) {       // function to move stepper
+  int delayTime = 20;   // speed of the motor, editable, the higher the delay, the slower the motor
   int stepCount;
-  const int (*sequence)[4]; // Puntatore alla sequenza corretta
+  const int (*sequence)[4]; // pointer to the drive
 
-  // Selezione della modalità di funzionamento
+  // selection of the drive
   if (metodo == "wave") {
     sequence = waveDrive;
     stepCount = 4;
@@ -89,7 +88,7 @@ void moveStepper(int passi, String metodo) {
     stepCount = 8;
   }
 
-  // Movimento del motore
+  // movement of the motor
   for (int i = 0; i < passi; i++) {
     int stepIndex = i % stepCount;
     digitalWrite(IN1, sequence[stepIndex][0]);
@@ -99,14 +98,14 @@ void moveStepper(int passi, String metodo) {
     delay(delayTime);
   }
 
-  // Spegne i pin per evitare surriscaldamento
+  // the pins are turned off to avoid overheating
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, LOW);
 }
 
-// Gestione richiesta HTTP per lo stepper
+// HTTP management
 void handleStepper() {
   if (server.hasArg("passi") && server.hasArg("metodo")) {
     int passi = server.arg("passi").toInt();
@@ -121,31 +120,31 @@ void handleStepper() {
 void setup() {
   Serial.begin(115200);
 
-  // Configura i pin del motore
+  // pins configuration
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
-  // Connessione WiFi
+  // WiFi connection
   WiFi.begin(ssid, password);
   Serial.print("Connessione a WiFi...");
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(".");
   }
-  Serial.println("\nConnesso al WiFi!");
+  Serial.println("\nConnesso al WiFi!"); // print ip address
   Serial.print("Indirizzo IP: ");
   Serial.println(WiFi.localIP());
 
-  // Configura mDNS (Nome rete locale: esp32.local)
+  // mDNS configuration (example name: esp32.local)
   if (!MDNS.begin("esp32")) {
     Serial.println("Errore avvio mDNS");
     return;
   }
   Serial.println("mDNS avviato: http://esp32.local");
 
-  // Configura il server web
+  // web server configuration
   server.on("/", []() {
     server.send_P(200, "text/html", index_html);
   });
