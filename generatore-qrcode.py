ac2166295e5e111c4129d4f513f4e945900c0d0a
rezzullo <andrea.rezzani19@gmail.com>
2025-03-15 08:22:51 +0100

    .

--- generatore-qrcode.py
+++ generatore-qrcode.py
@@ -1,5 +1,5 @@
 import qrcode
 local_url = "https://github.com/rezzullo/motorestepper" #your url of github example: https://github.com/rezzullo/motorestepper
 qr = qrcode.make(local_url)
-qr.save("qrcode_documentazione_progetto.png")
+qr.save("qrcode_documentazione_progetto.png") #your file name example: qrcode_documentazione_progetto.png, the qrcode will be created in the project folder
 print("QR code generato e salvato come 'qrcode_documentazione.png'")
